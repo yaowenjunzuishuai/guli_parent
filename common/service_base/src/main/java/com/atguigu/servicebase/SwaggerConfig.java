cccae9aac41c5c6b49c77c1e63da1d62df578103
yaowenjun <a13080590801@163.com>
2021-10-30 11:54:21 +0800

    dd

---
+++ common/service_base/src/main/java/com/atguigu/servicebase/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.atguigu.servicebase;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration//配置类
+@EnableSwagger2 //swagger注解
+public class SwaggerConfig {
+
+    @Bean
+    public Docket webApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+
+    }
+
+    private ApiInfo webApiInfo(){
+
+        return new ApiInfoBuilder()
+                .title("五狐工作室")
+                .description("工作测试文档")
+                .version("1.0")
+                .contact(new Contact("小狐", "www.fivefox.html", "a13080590801@163.com"))
+                .build();
+    }
+}
---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduLoginController.java
@@ -0,0 +1,23 @@
+package com.atguigu.eduservice.controller;
+
+import com.atguigu.commonutils.R;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api("用户登录管理")
+@RestController
+@RequestMapping("eduservice/user")
+public class EduLoginController {
+    @PostMapping("login")
+    public R login(){
+        return R.ok().data("token","admin");
+    }
+
+    @GetMapping("info")
+    public R info(){
+        return R.ok().data("roles","[admin]").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+}
